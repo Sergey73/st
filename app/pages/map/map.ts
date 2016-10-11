c70e870962bd7020111cb6d1f2acefbc8df4c796
Sergey73 <hripchenkosergey@rambler.ru>
2016-10-12 01:28:10 +0400

    add navigation

--- app/pages/map/map.ts
+++ app/pages/map/map.ts
@@ -4,6 +4,7 @@ import { Fire } from '../../utils/fire';
 import { Geolocation } from 'ionic-native';
 
 declare var mapboxgl: any;
+declare var L: any;
 
 
 @Component({
@@ -11,8 +12,10 @@ declare var mapboxgl: any;
 })
 
 export class MapPage {
-  constructor(
-    private navCtrl: NavController, platform: Platform, private fire: Fire) {
+  public map: any;
+  public marker: any;
+
+  constructor(private navCtrl: NavController, platform: Platform, private fire: Fire) {
     platform.ready().then(() => {
       this.initPage();
     });
@@ -20,21 +23,50 @@ export class MapPage {
   }
 
   private initPage() {
-    Geolocation.getCurrentPosition().then((resp) => {
-    this.loadMap(resp.coords.longitude, resp.coords.latitude);
-    })
-    // this.loadMap(48.3113038, 54.3295861)
+    L.mapbox.accessToken = 'pk.eyJ1Ijoic2VyZ2V5NzMiLCJhIjoiY2lyM3JhYnAxMDAyeGh5bnFmczh3cTRseiJ9.KVe54Q2NCigy3J0j3didAA';
+    this.map = L.mapbox.map('map', 'mapbox.streets');
+
+    this.marker = L.marker([0, 0 ], {
+      draggable: true
+    });
+    this.marker.addTo(this.map);
+
+    let watch = Geolocation.watchPosition({
+      maximumAge: 2000, 
+      timeout: 5000, 
+      enableHighAccuracy: true
+    });
+
+    watch.subscribe( (resp) => {
+      // console.dir(resp);
+      // console.log('Latitude: '            + resp.coords.latitude          + '\n' +
+      //         'Longitude: '         + resp.coords.longitude         + '\n' +
+      //         'Altitude: '          + resp.coords.altitude          + '\n' +
+      //         'Accuracy: '          + resp.coords.accuracy          + '\n' +
+      //         'Altitude Accuracy: ' + resp.coords.altitudeAccuracy  + '\n' +
+      //         'Heading: '           + resp.coords.heading           + '\n' +
+      //         'Speed: '             + resp.coords.speed             + '\n' +
+      //         'Timestamp: '         + resp.timestamp                + '\n');
+      this.loadMap(resp.coords.latitude, resp.coords.longitude);
+    });
+      // this.loadMap(0,0);
   }
 
-  private loadMap(long, lat) {
-    mapboxgl.accessToken = 'pk.eyJ1Ijoic2VyZ2V5NzMiLCJhIjoiY2lyM3JhYnAxMDAyeGh5bnFmczh3cTRseiJ9.KVe54Q2NCigy3J0j3didAA';
+  // private loadMap(long, lat) {
+  //   mapboxgl.accessToken = 'pk.eyJ1Ijoic2VyZ2V5NzMiLCJhIjoiY2lyM3JhYnAxMDAyeGh5bnFmczh3cTRseiJ9.KVe54Q2NCigy3J0j3didAA';
 
-    new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v9',
-      center: [long, lat],
-      zoom: 11
-    }); 
+  //   new mapboxgl.Map({
+  //     container: 'map',
+  //     style: 'mapbox://styles/mapbox/streets-v9',
+  //     center: [long, lat],
+  //     zoom: 19
+  //   }); 
+  // }
+  private loadMap(lat, long) {
+
+    this.map.setView([lat, long]);
+    this.marker.setLatLng([lat, long]).update();
+
   }
-
 }
+ 
