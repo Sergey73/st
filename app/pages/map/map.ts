b89db364e4c0a5c68b4e8425afecd8abddee2799
Sergey73 <hripchenkosergey@rambler.ru>
2016-11-02 09:08:44 +0400

    track

--- app/pages/map/map.ts
+++ app/pages/map/map.ts
@@ -43,8 +43,8 @@ export class MapPage {
     this.map = L.mapbox.map('map', 'mapbox.streets', {
       drawControl: true,
       minZoom: 9,
-      maxBounds: [[54.46605, 48.08372], [53.86225, 50.21576]]
-    }).setView([54.311096, 48.3257941], 9);
+      // maxBounds: [[54.46605, 48.08372], [53.86225, 50.21576]]
+    }).setView([54.31109, 48.42499], 9);
 
     // координаты мыши показываем в tool
     this.map.on('mousemove', function(e) {
@@ -54,9 +54,12 @@ export class MapPage {
     this.marker = L.marker([54.4151707, 48.3257941], {
       draggable: true
     });
+
+    // добавили маркер
     this.marker.addTo(this.map);
     this.tool = ToolPage;
 
+    this.createTrack();
     // let watch = Geolocation.watchPosition({
     //   maximumAge: 2000, 
     //   timeout: 5000, 
@@ -85,6 +88,38 @@ export class MapPage {
     // }, 3000);
   }
 
+  private createTrack () {
+    let that = this;
+    let featureGroup = L.featureGroup().addTo(that.map);
+
+    let drawControl = new L.Control.Draw({
+      edit: {
+        featureGroup: featureGroup
+      },
+      draw: {
+        polygon: true,
+        polyline: false,
+        rectangle: false,
+        circle: false,
+        marker: false
+      }
+    }).addTo(that.map);
+
+    that.map.on('draw:created', showPolygonArea);
+    that.map.on('draw:edited', showPolygonAreaEdited);
+
+    function showPolygonAreaEdited(e) {
+      e.layers.eachLayer(function(layer) {
+        showPolygonArea({ layer: layer });
+      });
+    }
+    function showPolygonArea(e) {
+      featureGroup.clearLayers();
+      featureGroup.addLayer(e.layer);
+     // e.layer.bindPopup((LGeo.area(e.layer) / 1000000).toFixed(2) + ' km<sup>2</sup>');
+      e.layer.openPopup();
+    }
+  }
   // mapgl
   // private loadMap(long, lat) {
   //   mapboxgl.accessToken = 'pk.eyJ1Ijoic2VyZ2V5NzMiLCJhIjoiY2lyM3JhYnAxMDAyeGh5bnFmczh3cTRseiJ9.KVe54Q2NCigy3J0j3didAA';
